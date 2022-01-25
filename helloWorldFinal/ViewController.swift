ecbaacebabb73034764dd3a02fc2500530630a99
Isa Fontana <isfon8@gmail.com>
2022-01-25 14:14:07 -0700

    final product

--- helloWorldFinal/ViewController.swift
+++ helloWorldFinal/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var favoriteBand: UILabel!
+    @IBOutlet weak var numberOfMembersLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        favoriteBand.text = "Favorite Band"
     }
 
-
+    @IBAction func pressButton(_ sender: UIButton) {
+        favoriteBand.text = "88rising"
+        
+    }
+    
+    @IBAction func clickMeButton(_ sender: UIButton) {
+        numberOfMembersLabel.text = "88rising has 70 members"
+    }
 }
 
